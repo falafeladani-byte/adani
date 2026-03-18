a35531e9a158677e113f284c825a532a46c8c567
falafeladani-byte <falafel.adani@gmail.com>
2026-03-18 15:29:41 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-// ==========================================
-// 🔔 SERVICE WORKER - פלאפל עדני
-// ==========================================
-// קובץ זה חייב להיות ממוקם בתיקיית השורש של האתר
-// לצד index.html
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
